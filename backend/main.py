c6c204e39e7bbdfac190e47f57abc73664545cf5
moisemartial <moisemartialcheumadjeumoussi@gmail.com>
2026-02-19 12:26:31 +0100

    remplacement des appels api de db.Py par db_sqlite.py

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
 from Membre import Membre, MembreCreate
-from db import Database
 from db_sqlite import DatabaseSqlite
 
 app = FastAPI(title="Gestion Membre")
@@ -25,9 +24,10 @@ def root():
 
 
 def get_database():
-    db = Database()
+    db1 = DatabaseSqlite()
+    db1.initialisation_de_la_db()
 
-    return db
+    return db1
 
 
 """def get_database():
@@ -40,45 +40,49 @@ def get_database():
 
 
 @app.post("/api/membres", response_model=Membre)
-def creer_membre(membre: MembreCreate, db: Database = Depends(get_database)):
+def creer_membre(membre: MembreCreate,db1: DatabaseSqlite = Depends(get_database)):
+
+
+    db1.create_membre(membre)
+    membres = db1.get_all_membres()
     """Créer un nouveau membre"""
-    return db.create_membre(membre)
+    return membres[-1]
 
 
 @app.get("/api/membres", response_model=List[Membre])
-def lire_membres(db: Database = Depends(get_database)):
+def lire_membres(db1: DatabaseSqlite = Depends(get_database)):
     """Récupérer la liste des membres avec filtre optionnel"""
-    return db.get_all_membres()
+    return db1.get_all_membres()
 
 
 @app.get("/api/membres/{membre_id}", response_model=Membre)
-def lire_membre(membre_id: int, db: Database = Depends(get_database)):
-    result = db.get_membre_by_id(membre_id)
-    if not result:
+def lire_membre(membre_id: int,db1: DatabaseSqlite = Depends(get_database)):
+    result1 = db1.get_membre_by_id(membre_id)
+    if  not result1:
         raise HTTPException(status_code=404, detail="Membre non trouvé")
-    return result
+    return  result1
 
 
 @app.put("/api/membres/{membre_id}", response_model=Membre)
 def modifier_membre(
-    membre_id: int, membre_data: MembreCreate, db: Database = Depends(get_database)
+    membre_id: int, membre_data: MembreCreate,db1: DatabaseSqlite = Depends(get_database)
 ):
     """Modifier un membre existant"""
-    membre_modifie = db.update_membre(membre_id, membre_data)
-    if membre_modifie is None:
+    membre_modifie1 = db1.update_membre(membre_id, membre_data)
+    if  membre_modifie1 is None:
         raise HTTPException(status_code=404, detail="Membre non trouvé")
-    return membre_modifie
+    return membre_modifie1
 
 
 @app.delete("/api/membres/{membre_id}")
-def supprimer_membre(membre_id: int, db: Database = Depends(get_database)):
+def supprimer_membre(membre_id: int,db1: DatabaseSqlite = Depends(get_database)):
     """Supprimer un membre"""
-    if not db.delete_membre(membre_id):
+    if not db1.delete_membre(membre_id):
         raise HTTPException(status_code=404, detail="Membre non trouvé")
     return {"message": "Membre supprimé avec succès"}
 
 
 @app.get("/api/stats")
-def obtenir_stats(db: Database = Depends(get_database)):
+def obtenir_stats(db1: DatabaseSqlite = Depends(get_database)):
     """Récupérer les statistiques des membres"""
-    return db.get_stats()
+    return db1.get_stats()
